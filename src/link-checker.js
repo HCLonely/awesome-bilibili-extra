a07f789af9d38d18be0d992eca902ee5f9be1f88
HCLonely <h1606051253@gmail.com>
2023-06-27 10:43:41 +0800

    Update at 2023-06-27 10:43:41

--- src/link-checker.js
+++ src/link-checker.js
@@ -46,7 +46,7 @@ const axios = require('axios');
 
   if (errorLinks.length > 0) {
     console.table(errorLinks);
-    console.error(\n`${errorLinks.length} broken links were founded!`);
+    console.error(`\n${errorLinks.length} broken links were founded!`);
     process.exit(1);
   }
 
@@ -59,7 +59,7 @@ const axios = require('axios');
       }
       return {name: e[1], link: e[2]};
     }).filter(e => e));
-    console.error(\n`Double links were founded!`);
+    console.error(`\nDouble links were founded!`);
     process.exit(1);
   }
 })();
